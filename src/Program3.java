72aa554fe16c02ca6b7c1303a29365d97d4ab363
Afaq Ahmad <afaqaijaz66@gmail.com>
2025-09-13 14:29:30 -0500

    initial commit

---
+++ src/Program3.java
@@ -0,0 +1,65 @@
+//********************************************************************
+//
+//  Author:       <Your name>
+//
+//  Program #:    1
+//
+//  File Name:    ProgramTemplate.java
+//
+//  Course:       ITSE 2321 Object-Oriented Programming
+//
+//  Due Date:     09-21-2025
+//
+//  Instructor:   Prof. Fred Kumi 
+//
+//  Chapter:      Chapter 3:  Introduction to Classes
+//
+//  Description:  <An explanation of what the program is designed to do>
+//
+//********************************************************************
+
+
+public class Program3
+{
+    //***************************************************************
+    //
+    //  Method:       main
+    // 
+    //  Description:  The main method of the program
+    //
+    //  Parameters:   String array
+    //
+    //  Returns:      N/A 
+    //
+    //**************************************************************
+    public static void main(String[] args)
+    {
+
+
+
+        developerInfo();
+
+        // Enter more statements here to make
+        // the program work as expected
+
+    } // End of the main method
+
+    //***************************************************************
+    //
+    //  Method:       developerInfo
+    // 
+    //  Description:  The developer information method of the program
+    //
+    //  Parameters:   None
+    //
+    //  Returns:      N/A 
+    //
+    //**************************************************************
+    public static void developerInfo()
+    {
+        System.out.println("Name:     Afaq Ahmad");
+        System.out.println("Course:   ITSE 2321 Object-Oriented Programming");
+        System.out.println("Program:  Three");
+        System.out.println("Due Date: 09-21-2025\n");
+    } // End of the developerInfo method
+}
